223f4b1674cfa2f7a09312b089ebd460e4feacaa
cmolho <cmolho@macalester.edu>
2016-04-15 02:14:35 -0500

    added locations (probably buggy)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var printers = require('./routes/printers');
 var statuses = require('./routes/statuses');
+var locations = require('./routes/locations');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/printers', printers);
 app.use('/statuses', statuses);
+app.use('/locations', locations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/locations.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET locations page. */
+
+//================
+// mysql functions
+//================
+
+var mysql = require('mysql');
+
+var connection = mysql.createPool({
+    host		: 'ec2-54-213-132-214.us-west-2.compute.amazonaws.com',
+    user		: 'FixItMacServer',
+    password : 'serverpassword',
+    database : 'FixItMacDatabase',
+    port		: '3306',
+    multipleStatements: true
+});
+
+
+function getLocations(callback) {
+    connection.query('SELECT * FROM `locationCategory`',  function(err, results, fields) {
+        if (err) {
+            console.log('Error while performing location Query: ', err);
+            return callback(err,null);
+        }
+        console.log('Successful location query');
+        json = JSON.stringify(results);
+        return callback(null,json);
+    });
+}
+
+//================
+// express routing
+//================
+
+router.get('/locations', function(req,res) {
+    getLocations(function(request,response) {
+        json = response;
+        res.header('Access-Control-Allow-Origin', '*');
+        res.send(json);
+    });
+});
+
+module.exports = router;
+
