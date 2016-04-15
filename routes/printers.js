7216f758833d2b872a33aeb33778cfc2630d567c
cmolho <cmolho@macalester.edu>
2016-04-15 02:28:58 -0500

    added post functions to printers

--- routes/printers.js
+++ routes/printers.js
@@ -42,6 +42,28 @@ function getPrinterByID(id, callback) {
     });
 }
 
+function setIssue(id,issue,callback) {
+    connection.query('UPDATE `printer` SET printerStatus=0 WHERE printerID=?; UPDATE `status` SET ??=0 WHERE printerID=?;', [id,issue,id], function(err, results, fields) {
+        if (err) {
+            console.log('Error while setting printer issue: ', err);
+            return callback(err,null);
+        }
+        console.log('Successful printer status set to 0: \tPrinter #', id);
+        return callback(null,'success');
+    });
+}
+
+function setWorking(id,callback) {
+    connection.query('UPDATE `printer` SET printerStatus=1 WHERE printerID=?; UPDATE `status` SET inkStatus=1,paperStatus=1,jamStatus=1,otherStatus=1 WHERE printerID=?;', [id,id], function(err, results, fields) {
+        if (err) {
+            console.log('Error while setting printer working: ', err);
+            return callback(err,null);
+        }
+        console.log('Successful printer status set to 1: \tPrinter #', id);
+        return callback(null,'success');
+    });
+}
+
 //================
 // express routing
 //================
@@ -59,6 +81,22 @@ router.get('/:id', function(req,res) {
         json = response;
         res.header('Access-Control-Allow-Origin', '*');
         res.send(json);
+    });
+});
+
+router.post('/:id/setissue/:issue', function(req,res) {
+    setIssue(req.params.id,req.params.issue,function(request,response) {
+        json = response;
+        res.header('Access-Control-Allow-Origin', '*');
+        res.send('success');
+    });
+});
+
+router.post('/:id/setworking', function(req,res) {
+    setWorking(req.params.id,function(request,response) {
+        json = response;
+        res.header('Access-Control-Allow-Origin', '*');
+        res.send('success');
     });
 });
 
